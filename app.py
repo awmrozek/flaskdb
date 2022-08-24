38f5bea0ba8b59cb2dfdc9b5e378821eda83a013
awmrozek <mrozekss@gmail.com>
2022-08-24 18:43:47 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/<name>')
+def hello_name(name):
+    return f"Hello, {escape(name)}"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    return "The project page"
+
+@app.route('/about')
+def about():
+    return "The about page"
